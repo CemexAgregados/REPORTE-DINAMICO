18579984a5d8c90d3ba609e955f39d339b63b9aa
Roque Morales <roque.morales@ext.cemex.com>
2026-09-23 10:49:45 -0600

    Borrar archivos

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel("bitacora_sintetica.xlsx", sheet_name="Bitacora", header=[0, 1])
-cols = list(df.columns[2:])
-for mat in dict.fromkeys(top for top, _ in cols):
-    print(mat, [sub for top, sub in cols if top == mat], "\n")
